134fa54d17f0004fbba72719d6ba46df6b211ceb
Lucas Lima <lucasvinilima15@gmail.com>
2022-03-01 17:19:29 -0300

    class 2

--- class2/app.js
+++ class2/app.js
@@ -1,20 +1,21 @@
+const path = require('path');
+
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin' ,adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-    console.log("this aways runs");
-    next();
-})
-
-app.use('/product' ,(req, res, next) => {
-    console.log('In another middlerare');
-    res.send('<h1>Hello from product</h1>');
-});
-
-app.use('/' ,(req, res, next) => {
-    console.log('In another middlerare');
-    res.send('<h1>Hello from express</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
