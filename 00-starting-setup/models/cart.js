febaf284739a129dc35a6e3bf4ebfc185bfd6d64
Lucas Lima <lucasvinilima15@gmail.com>
2022-03-14 22:54:20 -0300

    deletando produto do cart

--- 00-starting-setup/models/cart.js
+++ 00-starting-setup/models/cart.js
@@ -45,6 +45,9 @@ module.exports = class Cart {
       }
       const updateCart = {...JSON.parse(cart)};
       const product = updateCart.products.find(prod => prod.id === id);
+      if (!product) {
+        return;
+      }
       const produtQty = product.qty;
       updateCart.products = updateCart.products.filter(prod => prod.id !== id);
       updateCart.totalPrice = updateCart.totalPrice - produtQty * productPrice;
@@ -52,6 +55,17 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(updateCart), err => {
         console.log(err);
       })
+    });
+  }
+
+  static getProducts(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart)
+      }
     });
   }
 };
