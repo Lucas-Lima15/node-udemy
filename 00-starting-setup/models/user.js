cacee454a9c5713fa6d45f1dd705d83314a3d328
Lucas Lima <lucasViniLima15@gmail.com>
2022-04-05 16:11:26 -0300

    creating new users

--- 00-starting-setup/models/user.js
+++ 00-starting-setup/models/user.js
@@ -1,21 +1,43 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor(username, email, id) {
+    this.username = username;
+    this.email = email;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
-});
+
+  save() {
+    const db = getDb();
+    let dbOp;
+
+    if (this_id) {
+      dbOp = db.collection('users').updateOne({_id: this._id}, {$set: this});
+    } else {
+      dbOp = db.collection('users').insertOne(this);
+    }
+    return dbOp
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection('users')
+      .find({_id: new mongodb.ObjectId(userId)})
+      .next()
+      .then(user => {
+        return user;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = User;
