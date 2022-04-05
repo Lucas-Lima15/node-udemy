29397da294264d5b5d84b86943e6cc759234d47e
Lucas Lima <lucasViniLima15@gmail.com>
2022-04-05 15:11:08 -0300

    Finishing the Update Product code

--- 00-starting-setup/controllers/admin.js
+++ 00-starting-setup/controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const mongodb = require('mongodb');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -54,14 +55,9 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
-  .then(product => {
-    product.title = updatedTitle;
-    product.price = updatedPrice;
-    product.imageUrl = updatedImageUrl;
-    product.description = updatedDesc;
-    return product.save();
-  })
+  
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, new mongodb.ObjectId(prodId));
+  product.save()
   .then(result => {
     console.log('UPDATED PRODUCT');
     res.redirect('/admin/products');
