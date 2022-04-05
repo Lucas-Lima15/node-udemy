0f374392ba6d6d1ab1b8b8f1eb81648f8ecd8c58
Lucas Lima <lucasViniLima15@gmail.com>
2022-04-05 15:24:19 -0300

    One note about Updating Products

--- 00-starting-setup/controllers/admin.js
+++ 00-starting-setup/controllers/admin.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const mongodb = require('mongodb');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -56,7 +55,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   
-  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, new mongodb.ObjectId(prodId));
+  const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
   product.save()
   .then(result => {
     console.log('UPDATED PRODUCT');
